644142e1f14f14ec031d342d4f7c1d6ad9c5fccc
zoroo30 <ahmed.zoroo30@outlook.com>
2021-09-26 19:05:30 +0200

    adding typescript - adding prettier config file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,27 @@
 module.exports = {
   root: true,
+
   env: {
     node: true,
   },
-  extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/airbnb',
-  ],
+
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: '@typescript-eslint/parser',
   },
+
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
+
   overrides: [
     {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)',
-      ],
+      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
       env: {
         jest: true,
       },
     },
   ],
+
+  extends: ['plugin:vue/vue3-essential', '@vue/airbnb', '@vue/typescript'],
 };
